db76c18cf82023824d9452a7bf43c14cabbee613
Ahmed S. Wasif <ahmed.wasif@toptal.com>
2023-01-21 15:22:50 +0600

    Add result display panel

---
+++ src/components/ResultPanel.tsx
@@ -0,0 +1,26 @@
+type ResultPanelProps = {
+  words: string[];
+};
+
+export default function ResultPanel({ words }: ResultPanelProps) {
+  if (words.length > 0) {
+    return (
+      <div className="mt-6">
+        <hr className="mb-3" />
+        <label className="block text-gray-700 text-lg font-bold mb-2">
+          Non English words:
+        </label>
+        <p className="text-red-700">{words.join(", ")}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-6">
+      <hr className="mb-3" />
+      <label className="block text-green-700 text-lg font-bold mb-2">
+        No non English words found.
+      </label>
+    </div>
+  );
+}
